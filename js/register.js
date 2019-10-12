037b0c99d3e0ab48cf073be6db557745cb54536a
Aravindhan V <varavindhan2010@gmail.com>
2019-10-12 16:59:02 +0530

    Register Page Bug Fixed

--- js/register.js
+++ js/register.js
@@ -1,5 +1,40 @@
-var x = document.forms["myForm"]["fname"].value;
-if (x == "") {
-    alert("Name must be filled out");
-    return false;
+// var x = document.forms["myForm"]["fname"].value;
+// if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+// }                
+
+var form = document.forms[0];
+
+function validateForm(){
+    // var name = form['name'].value;
+    // if(name == ""){
+    //     alert("Please Fill All Fields");
+    //     return false;
+    // }
+
+    // var email = form['email'].value;
+    // if(email == ""){
+    //     alert("Please Fill All Fields");
+    //     return false;
+    // }
+
+    var pass = form['pass'].value;
+    if(pass == ""){
+        alert("Please Fill All Fields");
+        return false;
+    }
+
+    var cpass = form['cpass'].value;
+    if(cpass != pass){
+        alert("Passwords Don't Match");
+        return false;
+    }
+
+    var age = form['age'].value;
+    console.log("Age: "+age);
+    if(age == ""){
+        alert("Please Fill All Fields");
+        return false;
+    }
 }
