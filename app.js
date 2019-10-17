86522f862343a56522eb52a45010781ddad147ec
Aravindhan V <varavindhan2010@gmail.com>
2019-10-17 18:21:53 +0530

    Results Page Done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mysql  = require('mysql');
 var bodyParser =  require('body-parser');
+var session = require('express-session');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -20,6 +21,7 @@ app.use(express.static("css"));
 app.use(express.static("js"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret: 'dbmsProject', resave: false, saveUninitialized: false}));
 
 app.get('/',function(req,res){
     res.render("home");
@@ -34,8 +36,8 @@ app.get("/register",function(req,res){
 });
 
 app.get('/home',function(req,res){
-    res.render('search');
-});
+    res.render("search");
+});;
 
 app.get('/search',function(req,res){
     var r = req.query;
@@ -44,16 +46,23 @@ app.get('/search',function(req,res){
     var date = r.date;
     var class_type = r.class;
     var noofppl = r.noofppl;
-    var sql = "select a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,f.departure_time,f.arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and c.airline_id = l.airline_id and from_airport_code='"+from+"' and to_airport_code = '"+to+"' and DATE(departure_time)='"+date+"' and f.seats_left_"+class_type+">0";
-    console.log(sql);
+    var sql = "select l.logo,"+noofppl+"*c."+class_type+" as price,a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,TIME(f.departure_time) as departure_time,TIME(f.arrival_time) as arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and c.airline_id = l.airline_id and from_airport_code='"+from+"' and to_airport_code = '"+to+"' and DATE(departure_time)='"+date+"' and f.seats_left_"+class_type+">="+noofppl;
+    // console.log(sql);
     connection.query(sql,function(err,result){
         if(err){
             console.log(err);
         }
         else{
-            res.render("results",{flights: result});
+            req.session.message = result;
+            res.redirect('/flights');
         }
     });
+});
+
+app.get('/flights',function(req,res){
+    var flights = req.session.message;
+    console.log(flights);
+    res.render("results",{flights: flights});
 });
 
 app.post("/login",function(req,res){
--- draft.sql
+++ draft.sql
@@ -12,7 +12,7 @@ DROP TABLE IF EXISTS bookings;
 
 CREATE TABLE login(email varchar(50) primary key, password varchar(30));
 CREATE TABLE user(email varchar(50) primary key, name varchar(30), age int, gender char(1));
-CREATE TABLE airlines(airline_id varchar(10) primary key, airline_name varchar(25));
+CREATE TABLE airlines(airline_id varchar(10) primary key, airline_name varchar(25), logo varchar(100));
 CREATE TABLE airports(airport_code varchar(25) primary key,airport_name varchar(50), country varchar(25));
 CREATE TABLE flights(flight_no varchar(25) primary key, from_airport_code varchar(25), to_airport_code varchar(25), airline_id varchar(25), departure_time datetime, arrival_time datetime, seats_left_economy int(5), seats_left_business int(5));
 CREATE TABLE costs(airline_id varchar(25), economy int(11), business int(11));
@@ -29,9 +29,9 @@ ALTER TABLE bookings ADD CONSTRAINT FOREIGN KEY(flight_no) REFERENCES flights(fl
 INSERT INTO airports values("BLR","Bengaluru International Airport","India");
 INSERT INTO airports values("MAA","Chennai International Airport","India");
 INSERT INTO airports values("BOM","Chhatrapati Shivaji International Airport","India");
-INSERT INTO airlines values("AIR001","Jet Airways");
-INSERT INTO airlines values("AIR002","SpiceJet");
-INSERT INTO flights values("JA1234","BLR","MAA","AIR001","2019-10-21 06:00:00","2019-10-21 08:00:00",50,20);
+INSERT INTO airlines values("AIR001","Jet Airways","https://pbs.twimg.com/profile_images/876775499799736321/1nFK_O5O_400x400.jpg");
+INSERT INTO airlines values("AIR002","SpiceJet","https://i.pinimg.com/originals/9f/41/91/9f4191ace7a83b42d468322d5668fefb.png");
+INSERT INTO flights values("JA1234","BLR","MAA","AIR002","2019-10-21 06:00:00","2019-10-21 08:00:00",50,20);
 INSERT INTO flights values("SJ1234","BLR","BOM","AIR001","2019-10-23 10:00:00","2019-10-23 16:00:00",100,250);
 INSERT INTO costs values("AIR001", 4500, 9000);
 INSERT INTO costs values("AIR002", 3000, 6000);
@@ -39,4 +39,4 @@ INSERT INTO costs values("AIR002", 3000, 6000);
 COMMIT;
 
 -- Comment All after this
--- select a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,f.depature_time,f.arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and l.airline_id = c.airline_id;
+-- select l.logo,6*c.business as price,a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,TIME(f.departure_time) as departure_time,TIME(f.arrival_time) as arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and c.airline_id = l.airline_id and from_airport_code='BLR' and to_airport_code = 'MAA' and DATE(departure_time)='2019-10-21' and f.seats_left_business>=5;
