e4c9f57c9743e64cf8c9638ca477470d569134ea
Aravindhan V <varavindhan2010@gmail.com>
2019-10-24 14:43:59 +0530

    Resolved Conflicts

--- app.js
+++ app.js
@@ -1,114 +1,150 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var mysql  = require('mysql');
-var bodyParser =  require('body-parser');
-var session = require('express-session');
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'name',
-    password: 'MistakenFoe-6',
-    database: 'airline'
+  host: "localhost",
+  user: "dbms",
+  password: "dbms",
+  database: "airline"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to MYSQL!");
-  });
+  if (err) throw err;
+  console.log("Connected to MYSQL!");
+});
 
 app.use(express.static("assets"));
 app.use(express.static("css"));
 app.use(express.static("js"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({secret: 'dbmsProject', resave: false, saveUninitialized: false}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({ secret: "dbmsProject", resave: false, saveUninitialized: false })
+);
 
-app.get('/',function(req,res){
-    res.render("home");
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-app.get('/login',function(req,res){
-    res.render("home");
+app.get("/login", function(req, res) {
+  res.render("home");
 });
 
-app.get("/register",function(req,res){
-    res.render("register");
+app.get("/register", function(req, res) {
+  res.render("register");
 });
 
-app.get('/home',function(req,res){
-    res.render("search");
-});;
+app.get("/home", function(req, res) {
+  res.render("search");
+});
 
-app.get('/search',function(req,res){
-    var r = req.query;
-    var from = r.from;
-    var to = r.to;
-    var date = r.date;
-    var class_type = r.class;
-    var noofppl = r.noofppl;
-    var sql = "select l.logo,"+noofppl+"*c."+class_type+" as price,a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,TIME(f.departure_time) as departure_time,TIME(f.arrival_time) as arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and c.airline_id = l.airline_id and from_airport_code='"+from+"' and to_airport_code = '"+to+"' and DATE(departure_time)='"+date+"' and f.seats_left_"+class_type+">="+noofppl;
-    // console.log(sql);
-    connection.query(sql,function(err,result){
-        if(err){
-            console.log(err);
-        }
-        else{
-            req.session.message = result;
-            res.redirect('/flights');
-        }
+app.get("/search", function(req, res) {
+  var r = req.query;
+  var from = r.from;
+  var to = r.to;
+  var date = r.date;
+  var class_type = r.class;
+  var noofppl = r.noofppl;
+  req.session.noofppl = noofppl;
+  var sql =
+    "select l.logo," +
+    noofppl +
+    "*c." +
+    class_type +
+    " as price,a1.airport_code as from_code,a1.airport_name as from_name,a2.airport_code as to_code,a2.airport_name as to_name,l.airline_name,f.flight_no,TIME(f.departure_time) as departure_time,TIME(f.arrival_time) as arrival_time from airports as a1, airports as a2, airlines as l, flights as f,costs as c where l.airline_id=f.airline_id and f.from_airport_code=a1.airport_code and f.to_airport_code=a2.airport_code and c.airline_id = l.airline_id and from_airport_code='" +
+    from +
+    "' and to_airport_code = '" +
+    to +
+    "' and DATE(departure_time)='" +
+    date +
+    "' and f.seats_left_" +
+    class_type +
+    ">=" +
+    noofppl;
+  // console.log(sql);
+  connection.query(sql, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      req.session.message = result;
+      res.redirect("/flights");
+    }
+  });
+});
+
+app.get("/flights", function(req, res) {
+  var flights = req.session.message;
+  console.log(flights);
+  res.render("results", { flights: flights });
+});
+
+app.get("/test", function(req, res) {
+  res.render("passenger");
+});
+
+app.get("/book/:flight_id", function(req, res) {
+  var n = req.session.noofppl;
+  req.session.f = 1;
+  if (f > n) {
+    res.render("passenger",{n:f});
+  } else {
+    res.send("You have Booked " + req.params.flight_id);
+  }
+});
+
+app.post("/passenger",function(req,res){
+	
+});
+
+app.post("/login", function(req, res) {
+  var body = req.body;
+  var email = body.email;
+  var pass = body.pass;
+  var getPass = "SELECT password FROM login WHERE email='" + email + "'";
+
+  connection.query(getPass, function(err, result, fields) {
+    if (err) {
+      console.log(err);
+    } else {
+      var dbpass = result[0].password;
+      if (pass == dbpass) {
+        res.redirect("/home");
+      } else {
+        res.redirect("/login");
+      }
+    }
+  });
+});
+
+app.post("/register", function(req, res) {
+  var body = req.body;
+  var email = body.email;
+  var pass = body.pass;
+  var name = body.name;
+  var age = body.age;
+  var gender = body.gender;
+
+  var sql = "INSERT INTO login VALUES('" + email + "','" + pass + "')";
+  var datasql =
+    "INSERT INTO user VALUES('" +
+    email +
+    "','" +
+    name +
+    "'," +
+    age +
+    ",'" +
+    gender +
+    "')";
+  connection.query(sql, function(err, result) {
+    connection.query(datasql, function(ierr, iresult) {
+      if (ierr) throw ierr;
+      console.log(iresult);
+      res.redirect("/login");
     });
-});
-
-app.get('/flights',function(req,res){
-    var flights = req.session.message;
-    console.log(flights);
-    res.render("results",{flights: flights});
-});
-
-app.get('/book/:flight_id',function(req,res){
-    res.send("You have Booked "+req.params.flight_id);
-});
-
-app.post("/login",function(req,res){
-    var body = req.body;
-    var email = body.email;
-    var pass = body.pass;
-    var getPass = "SELECT password FROM login WHERE email='"+email+"'";
-
-    connection.query(getPass,function(err,result, fields){
-        if(err){
-            console.log(err);
-        }
-        else{
-            var dbpass = result[0].password;
-            if(pass == dbpass){
-                
-                res.redirect('/home');
-            }
-            else{
-                res.redirect('/login');
-            }
-        }
-    });
-});
-
-app.post("/register",function(req,res){
-    var body = req.body;
-    var email = body.email;
-    var pass = body.pass;
-    var name = body.name;
-    var age = body.age;
-    var gender = body.gender;
-
-    var sql = "INSERT INTO login VALUES('"+email+"','"+pass+"')";
-    var datasql = "INSERT INTO user VALUES('"+email+"','"+name+"',"+age+",'"+gender+"')";
-    connection.query(sql,function(err,result){
-        connection.query(datasql,function(ierr,iresult){
-            if(ierr) throw ierr;
-            console.log(iresult);
-            res.redirect('/login');
-        });
-    });
+  });
 });
 
 app.listen(8080,function(){
--- draft.sql
+++ draft.sql
@@ -16,8 +16,8 @@ CREATE TABLE airlines(airline_id varchar(10) primary key, airline_name varchar(2
 CREATE TABLE airports(airport_code varchar(25) primary key,airport_name varchar(50), country varchar(25));
 CREATE TABLE flights(flight_no varchar(25) primary key, from_airport_code varchar(25), to_airport_code varchar(25), airline_id varchar(25), departure_time datetime, arrival_time datetime, seats_left_economy int(5), seats_left_business int(5));
 CREATE TABLE costs(airline_id varchar(25), economy int(11), business int(11));
-CREATE TABLE bookings(booking_id varchar(25) auto_increment, customer_email varchar(50),no_of_seats int(11), flight_no varchar(25), booking_date date, primary key(booking_id));
-CREATE TABLE passenger(booking_id varchar(25), customer_name varchar(25), gender char(1),age int(11));
+CREATE TABLE bookings(booking_id int(25) auto_increment, customer_email varchar(50),no_of_seats int(11), flight_no varchar(25), booking_date date, primary key(booking_id));
+CREATE TABLE passenger(booking_id int(25), customer_name varchar(25), gender char(1),age int(11));
 
 ALTER TABLE user ADD CONSTRAINT FOREIGN KEY(email) REFERENCES login(email) on delete cascade;
 ALTER TABLE flights ADD CONSTRAINT FOREIGN KEY(from_airport_code) REFERENCES airports(airport_code) on delete cascade;
